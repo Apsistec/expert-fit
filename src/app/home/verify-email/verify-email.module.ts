c72154f08482e44890a1a58fbcf0fd0dbe0ec8f8
Doug White <doug@apsistec.app>
2021-02-08 18:01:52 -0600

    verify email on again

--- src/app/home/verify-email/verify-email.module.ts
+++ src/app/home/verify-email/verify-email.module.ts
@@ -1,13 +1,13 @@
-// import { CommonModule } from '@angular/common';
-// import { NgModule } from '@angular/core';
-// import { FormsModule } from '@angular/forms';
-// import { IonicModule } from '@ionic/angular';
-// import { SharedModule } from '../../shared/shared.module';
-// import { VerifyEmailRoutingModule } from './verify-email-routing.module';
-// import { VerifyEmailPage } from './verify-email.page';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { SharedModule } from '../../shared/shared.module';
+import { VerifyEmailRoutingModule } from './verify-email-routing.module';
+import { VerifyEmailPage } from './verify-email.page';
 
-// @NgModule({
-//   imports: [CommonModule, FormsModule, IonicModule, VerifyEmailRoutingModule, SharedModule],
-//   declarations: [VerifyEmailPage]
-// })
-// export class VerifyEmailModule {}
+@NgModule({
+  imports: [CommonModule, FormsModule, IonicModule, VerifyEmailRoutingModule, SharedModule],
+  declarations: [VerifyEmailPage]
+})
+export class VerifyEmailModule {}
